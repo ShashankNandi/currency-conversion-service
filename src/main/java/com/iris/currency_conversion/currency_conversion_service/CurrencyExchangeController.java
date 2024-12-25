28c2921cd60ec1fe6caa33d01e5a9c340f6c855a
shashank189612 <shashanknm18@gmail.com>
2024-12-25 20:56:24 +0530

    implemented feign for calling the currency exchange service using the currencyExchangeproxy

--- src/main/java/com/iris/currency_conversion/currency_conversion_service/CurrencyExchangeController.java
+++ src/main/java/com/iris/currency_conversion/currency_conversion_service/CurrencyExchangeController.java
@@ -1,5 +1,6 @@
 package com.iris.currency_conversion.currency_conversion_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,14 +12,27 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
+
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion getConvertedCurrency(@PathVariable("from") String from,   @PathVariable("to") String to,   @PathVariable("quantity") int quantity) {
         CurrencyConversion  cnv= new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, from, to).getBody();
         BigDecimal qty=BigDecimal.valueOf(quantity);
         BigDecimal calculatedAmt=qty.multiply( cnv.getConversionMultiple());
-        return new CurrencyConversion(cnv.getId(), from, to, qty, cnv.getConversionMultiple(),calculatedAmt, cnv.getEnvironment());
+        return new CurrencyConversion(cnv.getId(), from, to, qty, cnv.getConversionMultiple(),calculatedAmt, cnv.getEnvironment()+" rest template");
 
 //        return new CurrencyConversion(1000L,from, to, BigDecimal.valueOf(quantity),BigDecimal.valueOf(85),  BigDecimal.valueOf(85*quantity)  );
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion getConvertedCurrency2(@PathVariable("from") String from,   @PathVariable("to") String to,   @PathVariable("quantity") int quantity) {
+        CurrencyConversion cnv = proxy.getCurrencyExchange(from, to);
+        BigDecimal qty=BigDecimal.valueOf(quantity);
+        BigDecimal calculatedAmt=qty.multiply( cnv.getConversionMultiple());
+        return new CurrencyConversion(cnv.getId(), from, to, qty, cnv.getConversionMultiple(),calculatedAmt, cnv.getEnvironment()+" feign");
+
     }
 }
