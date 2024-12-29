683eebf9565c1983781fa37e52416a435a88a126
shashank189612 <shashanknm18@gmail.com>
2024-12-29 16:22:26 +0530

    configured load balancing for different exchange services registered on eureka server

--- src/main/java/com/iris/currency_conversion/currency_conversion_service/CurrencyExchangeProxy.java
+++ src/main/java/com/iris/currency_conversion/currency_conversion_service/CurrencyExchangeProxy.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000") //this annotation makes req to currency exchange service running on port 8000
+@FeignClient(name = "currency-exchange")  // this makes request to any currency exchange service running and by default the load balancer on feign balances the load b/w differenet instances of the exchange service
+
 public interface CurrencyExchangeProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
